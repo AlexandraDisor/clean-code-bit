6e214e49be0919ea2e3f00a9d270a783febec702
AlexandraDisor <IoanaAlexandra.Disor@worldpay.com>
2022-03-30 18:43:19 +0300

    Initial project creation

---
+++ src/test/java/com/endava/cleancodebit/CleanCodeBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.cleancodebit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeBitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
